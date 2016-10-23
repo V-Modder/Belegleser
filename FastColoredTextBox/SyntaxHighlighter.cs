03a257f43df6331483b6a2298c9fa810db385106
V-Modder <V-Modder@web.de>
2016-10-23 17:47:15 +0200

    Added DotLiquid text highlighting

---
+++ FastColoredTextBox/SyntaxHighlighter.cs
@@ -0,0 +1,458 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace FastColoredTextBoxNS
+{
+    public class SyntaxHighlighter : IDisposable
+    {
+        //styles
+        protected static readonly Platform platformType = PlatformType.GetOperationSystemPlatform();
+        public readonly Style BlueBoldStyle = new TextStyle(Brushes.Blue, null, FontStyle.Bold);
+        public readonly Style BlueStyle = new TextStyle(Brushes.Blue, null, FontStyle.Regular);
+        public readonly Style BoldStyle = new TextStyle(null, null, FontStyle.Bold | FontStyle.Underline);
+        public readonly Style BrownStyle = new TextStyle(Brushes.Brown, null, FontStyle.Italic);
+        public readonly Style GrayStyle = new TextStyle(Brushes.Gray, null, FontStyle.Regular);
+        public readonly Style GreenStyle = new TextStyle(Brushes.Green, null, FontStyle.Italic);
+        public readonly Style MagentaStyle = new TextStyle(Brushes.Magenta, null, FontStyle.Regular);
+        public readonly Style MaroonStyle = new TextStyle(Brushes.Maroon, null, FontStyle.Regular);
+        public readonly Style RedStyle = new TextStyle(Brushes.Red, null, FontStyle.Regular);
+        public readonly Style BlackStyle = new TextStyle(Brushes.Black, null, FontStyle.Regular);
+        //
+        protected readonly Dictionary<string, SyntaxDescriptor> descByXMLfileNames = new Dictionary<string, SyntaxDescriptor>();
+
+        public static RegexOptions RegexCompiledOption
+        {
+            get
+            {
+                if (platformType == Platform.X86)
+                    return RegexOptions.Compiled;
+                else
+                    return RegexOptions.None;
+            }
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            foreach (SyntaxDescriptor desc in descByXMLfileNames.Values)
+                desc.Dispose();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Highlights syntax for given language
+        /// </summary>
+        public virtual void HighlightSyntax(Language language, Range range)
+        {
+            switch (language)
+            {
+                case Language.DotLiquid:
+                    DotLiquidHighlight(range);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="range"></param>
+        public virtual void DotLiquidHighlight(Range range)
+        {
+        }
+
+        /// <summary>
+        /// Highlights syntax for given XML description file
+        /// </summary>
+        public virtual void HighlightSyntax(string XMLdescriptionFile, Range range)
+        {
+            SyntaxDescriptor desc = null;
+            if (!descByXMLfileNames.TryGetValue(XMLdescriptionFile, out desc))
+            {
+                var doc = new XmlDocument();
+                string file = XMLdescriptionFile;
+                if (!File.Exists(file))
+                    file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Path.GetFileName(file));
+
+                doc.LoadXml(File.ReadAllText(file));
+                desc = ParseXmlDescription(doc);
+                descByXMLfileNames[XMLdescriptionFile] = desc;
+            }
+
+            HighlightSyntax(desc, range);
+        }
+
+        public virtual void AutoIndentNeeded(object sender, AutoIndentEventArgs args)
+        {
+            //var tb = (sender as FastColoredTextBox);
+            //Language language = tb.Language;
+            //switch (language)
+            //{
+            //    case Language.CSharp:
+            //        CSharpAutoIndentNeeded(sender, args);
+            //        break;
+            //    case Language.VB:
+            //        VBAutoIndentNeeded(sender, args);
+            //        break;
+            //    case Language.HTML:
+            //        HTMLAutoIndentNeeded(sender, args);
+            //        break;
+            //    case Language.XML:
+            //        XMLAutoIndentNeeded(sender, args);
+            //        break;
+            //    case Language.SQL:
+            //        SQLAutoIndentNeeded(sender, args);
+            //        break;
+            //    case Language.PHP:
+            //        PHPAutoIndentNeeded(sender, args);
+            //        break;
+            //    case Language.JS:
+            //        CSharpAutoIndentNeeded(sender, args);
+            //        break; //JS like C#
+            //    case Language.Lua:
+            //        LuaAutoIndentNeeded(sender, args);
+            //        break;
+            //    default:
+            //        break;
+            //}
+        }
+
+        /// <summary>
+        /// Uses the given <paramref name="doc"/> to parse a XML description and adds it as syntax descriptor. 
+        /// The syntax descriptor is used for highlighting when 
+        /// <list type="bullet">
+        ///     <item>Language property of FCTB is set to <see cref="Language.Custom"/></item>
+        ///     <item>DescriptionFile property of FCTB has the same value as the method parameter <paramref name="descriptionFileName"/></item>
+        /// </list>
+        /// </summary>
+        /// <param name="descriptionFileName">Name of the description file</param>
+        /// <param name="doc">XmlDocument to parse</param>
+        public virtual void AddXmlDescription(string descriptionFileName, XmlDocument doc) {
+            SyntaxDescriptor desc = ParseXmlDescription(doc);
+            descByXMLfileNames[descriptionFileName] = desc;
+        }
+
+        public static SyntaxDescriptor ParseXmlDescription(XmlDocument doc)
+        {
+            var desc = new SyntaxDescriptor();
+            XmlNode brackets = doc.SelectSingleNode("doc/brackets");
+            if (brackets != null)
+            {
+                if (brackets.Attributes["left"] == null || brackets.Attributes["right"] == null ||
+                    brackets.Attributes["left"].Value == "" || brackets.Attributes["right"].Value == "")
+                {
+                    desc.leftBracket = '\x0';
+                    desc.rightBracket = '\x0';
+                }
+                else
+                {
+                    desc.leftBracket = brackets.Attributes["left"].Value[0];
+                    desc.rightBracket = brackets.Attributes["right"].Value[0];
+                }
+
+                if (brackets.Attributes["left2"] == null || brackets.Attributes["right2"] == null ||
+                    brackets.Attributes["left2"].Value == "" || brackets.Attributes["right2"].Value == "")
+                {
+                    desc.leftBracket2 = '\x0';
+                    desc.rightBracket2 = '\x0';
+                }
+                else
+                {
+                    desc.leftBracket2 = brackets.Attributes["left2"].Value[0];
+                    desc.rightBracket2 = brackets.Attributes["right2"].Value[0];
+                }
+
+                if (brackets.Attributes["strategy"] == null || brackets.Attributes["strategy"].Value == "")
+                    desc.bracketsHighlightStrategy = BracketsHighlightStrategy.Strategy2;
+                else
+                    desc.bracketsHighlightStrategy = (BracketsHighlightStrategy)Enum.Parse(typeof(BracketsHighlightStrategy), brackets.Attributes["strategy"].Value);
+            }
+
+            var styleByName = new Dictionary<string, Style>();
+
+            foreach (XmlNode style in doc.SelectNodes("doc/style"))
+            {
+                Style s = ParseStyle(style);
+                styleByName[style.Attributes["name"].Value] = s;
+                desc.styles.Add(s);
+            }
+            foreach (XmlNode rule in doc.SelectNodes("doc/rule"))
+                desc.rules.Add(ParseRule(rule, styleByName));
+            foreach (XmlNode folding in doc.SelectNodes("doc/folding"))
+                desc.foldings.Add(ParseFolding(folding));
+
+            return desc;
+        }
+
+        protected static FoldingDesc ParseFolding(XmlNode foldingNode)
+        {
+            var folding = new FoldingDesc();
+            //regex
+            folding.startMarkerRegex = foldingNode.Attributes["start"].Value;
+            folding.finishMarkerRegex = foldingNode.Attributes["finish"].Value;
+            //options
+            XmlAttribute optionsA = foldingNode.Attributes["options"];
+            if (optionsA != null)
+                folding.options = (RegexOptions)Enum.Parse(typeof(RegexOptions), optionsA.Value);
+
+            return folding;
+        }
+
+        protected static RuleDesc ParseRule(XmlNode ruleNode, Dictionary<string, Style> styles)
+        {
+            var rule = new RuleDesc();
+            rule.pattern = ruleNode.InnerText;
+            //
+            XmlAttribute styleA = ruleNode.Attributes["style"];
+            XmlAttribute optionsA = ruleNode.Attributes["options"];
+            //Style
+            if (styleA == null)
+                throw new Exception("Rule must contain style name.");
+            if (!styles.ContainsKey(styleA.Value))
+                throw new Exception("Style '" + styleA.Value + "' is not found.");
+            rule.style = styles[styleA.Value];
+            //options
+            if (optionsA != null)
+                rule.options = (RegexOptions)Enum.Parse(typeof(RegexOptions), optionsA.Value);
+
+            return rule;
+        }
+
+        protected static Style ParseStyle(XmlNode styleNode)
+        {
+            XmlAttribute typeA = styleNode.Attributes["type"];
+            XmlAttribute colorA = styleNode.Attributes["color"];
+            XmlAttribute backColorA = styleNode.Attributes["backColor"];
+            XmlAttribute fontStyleA = styleNode.Attributes["fontStyle"];
+            XmlAttribute nameA = styleNode.Attributes["name"];
+            //colors
+            SolidBrush foreBrush = null;
+            if (colorA != null)
+                foreBrush = new SolidBrush(ParseColor(colorA.Value));
+            SolidBrush backBrush = null;
+            if (backColorA != null)
+                backBrush = new SolidBrush(ParseColor(backColorA.Value));
+            //fontStyle
+            FontStyle fontStyle = FontStyle.Regular;
+            if (fontStyleA != null)
+                fontStyle = (FontStyle)Enum.Parse(typeof(FontStyle), fontStyleA.Value);
+
+            return new TextStyle(foreBrush, backBrush, fontStyle);
+        }
+
+        protected static Color ParseColor(string s)
+        {
+            if (s.StartsWith("#"))
+            {
+                if (s.Length <= 7)
+                    return Color.FromArgb(255,
+                                          Color.FromArgb(Int32.Parse(s.Substring(1), NumberStyles.AllowHexSpecifier)));
+                else
+                    return Color.FromArgb(Int32.Parse(s.Substring(1), NumberStyles.AllowHexSpecifier));
+            }
+            else
+                return Color.FromName(s);
+        }
+
+        public void HighlightSyntax(SyntaxDescriptor desc, Range range)
+        {
+            //set style order
+            range.tb.ClearStylesBuffer();
+            for (int i = 0; i < desc.styles.Count; i++)
+                range.tb.Styles[i] = desc.styles[i];
+            //brackets
+            char[] oldBrackets = RememberBrackets(range.tb);
+            range.tb.LeftBracket = desc.leftBracket;
+            range.tb.RightBracket = desc.rightBracket;
+            range.tb.LeftBracket2 = desc.leftBracket2;
+            range.tb.RightBracket2 = desc.rightBracket2;
+            //clear styles of range
+            range.ClearStyle(desc.styles.ToArray());
+            //highlight syntax
+            foreach (RuleDesc rule in desc.rules)
+                range.SetStyle(rule.style, rule.Regex);
+            //clear folding
+            range.ClearFoldingMarkers();
+            //folding markers
+            foreach (FoldingDesc folding in desc.foldings)
+                range.SetFoldingMarkers(folding.startMarkerRegex, folding.finishMarkerRegex, folding.options);
+
+            //
+            RestoreBrackets(range.tb, oldBrackets);
+        }
+
+        protected void RestoreBrackets(FastColoredTextBox tb, char[] oldBrackets)
+        {
+            tb.LeftBracket = oldBrackets[0];
+            tb.RightBracket = oldBrackets[1];
+            tb.LeftBracket2 = oldBrackets[2];
+            tb.RightBracket2 = oldBrackets[3];
+        }
+
+        protected char[] RememberBrackets(FastColoredTextBox tb)
+        {
+            return new[] { tb.LeftBracket, tb.RightBracket, tb.LeftBracket2, tb.RightBracket2 };
+        }
+
+        public void InitStyleSchema(Language lang)
+        {
+            switch (lang)
+            {
+                case Language.DotLiquid:
+                    StringStyle = RedStyle;
+                    CommentStyle = GreenStyle;
+                    NumberStyle = MagentaStyle;
+                    KeywordStyle = BlueBoldStyle;
+                    StatementsStyle = BlueBoldStyle;
+                    FunctionsStyle = MaroonStyle;
+                    VariableStyle = MaroonStyle;
+                    TypesStyle = BrownStyle;
+                    break;
+            }
+        }
+
+        #region Styles
+
+        /// <summary>
+        /// String style
+        /// </summary>
+        public Style StringStyle { get; set; }
+
+        /// <summary>
+        /// Comment style
+        /// </summary>
+        public Style CommentStyle { get; set; }
+
+        /// <summary>
+        /// Number style
+        /// </summary>
+        public Style NumberStyle { get; set; }
+
+        /// <summary>
+        /// C# attribute style
+        /// </summary>
+        public Style AttributeStyle { get; set; }
+
+        /// <summary>
+        /// Class name style
+        /// </summary>
+        public Style ClassNameStyle { get; set; }
+
+        /// <summary>
+        /// Keyword style
+        /// </summary>
+        public Style KeywordStyle { get; set; }
+
+        /// <summary>
+        /// Style of tags in comments of C#
+        /// </summary>
+        public Style CommentTagStyle { get; set; }
+
+        /// <summary>
+        /// HTML attribute value style
+        /// </summary>
+        public Style AttributeValueStyle { get; set; }
+
+        /// <summary>
+        /// HTML tag brackets style
+        /// </summary>
+        public Style TagBracketStyle { get; set; }
+
+        /// <summary>
+        /// HTML tag name style
+        /// </summary>
+        public Style TagNameStyle { get; set; }
+
+        /// <summary>
+        /// HTML Entity style
+        /// </summary>
+        public Style HtmlEntityStyle { get; set; }
+
+        /// <summary>
+        /// XML attribute style
+        /// </summary>
+        public Style XmlAttributeStyle { get; set; }
+
+        /// <summary>
+        /// XML attribute value style
+        /// </summary>
+        public Style XmlAttributeValueStyle { get; set; }
+
+        /// <summary>
+        /// XML tag brackets style
+        /// </summary>
+        public Style XmlTagBracketStyle { get; set; }
+
+        /// <summary>
+        /// XML tag name style
+        /// </summary>
+        public Style XmlTagNameStyle { get; set; }
+
+        /// <summary>
+        /// XML Entity style
+        /// </summary>
+        public Style XmlEntityStyle { get; set; }
+
+        /// <summary>
+        /// XML CData style
+        /// </summary>
+        public Style XmlCDataStyle { get; set; }
+
+        /// <summary>
+        /// Variable style
+        /// </summary>
+        public Style VariableStyle { get; set; }
+
+        /// <summary>
+        /// Specific PHP keyword style
+        /// </summary>
+        public Style KeywordStyle2 { get; set; }
+
+        /// <summary>
+        /// Specific PHP keyword style
+        /// </summary>
+        public Style KeywordStyle3 { get; set; }
+
+        /// <summary>
+        /// SQL Statements style
+        /// </summary>
+        public Style StatementsStyle { get; set; }
+
+        /// <summary>
+        /// SQL Functions style
+        /// </summary>
+        public Style FunctionsStyle { get; set; }
+
+        /// <summary>
+        /// SQL Types style
+        /// </summary>
+        public Style TypesStyle { get; set; }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Language
+    /// </summary>
+    public enum Language
+    {
+        Custom,
+        CSharp,
+        VB,
+        HTML,
+        XML,
+        SQL,
+        PHP,
+        JS,
+        Lua,
+        DotLiquid
+    }
+}
